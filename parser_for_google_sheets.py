59ce0d2e92d2fa1903bfed59b6bfd9f8e736030e
milanpetrovic87 <50910654+milanpetrovic87@users.noreply.github.com>
2023-09-26 08:58:20 +0200

    Update parser_for_google_sheets.py

--- parser_for_google_sheets.py
+++ parser_for_google_sheets.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import copy
-import openpyxl
 
-# https://docs.google.com/spreadsheets/d/1mrlE1Cz1yLwVSan3bxUwQs1GocnuWm371FdkCQVDHyg/edit?usp=sharing
 
 dfs = {}
 
+# https://docs.google.com/spreadsheets/d/1PYq9Run8wV6526g2WMpueoWDyPu2cPsD8XodhmuJKH0/edit?usp=sharing
 
-googleSheetId = '1mrlE1Cz1yLwVSan3bxUwQs1GocnuWm371FdkCQVDHyg'
+googleSheetId = '1PYq9Run8wV6526g2WMpueoWDyPu2cPsD8XodhmuJKH0'
 for worksheetName in ["Grupa1","Grupa2","Grupa3","Grupa4"]:
 
     URL = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(
@@ -19,32 +18,13 @@ for worksheetName in ["Grupa1","Grupa2","Grupa3","Grupa4"]:
     df = df.fillna('')
     dfs[worksheetName] = df
 
-# Load the Excel file
-# excel_file = pd.ExcelFile('tabela.xlsx')
-
-# Get the sheet names
-sheet_names = [ 'Grupa 1' , 'Grupa 2', 'Grupa 3', 'Grupa 4' ]
-
-# Create a dictionary to hold DataFrames
-dfs = {}
-
-# Iterate through the sheets and convert them to DataFrames
-for sheet_name in sheet_names:
-    df = excel_file.parse(sheet_name)
-    df = df.fillna('')
-    dfs[sheet_name] = df
-
-# Now, dfs dictionary contains DataFrames with sheet names as keys
-# You can access them like dfs['Sheet1'], dfs['Sheet2'], etc.
-
-
 
 
 for sheet_name, df in dfs.items():
  
     # Empty new dict
     newdf = {
-    "Igrac":[],
+    "Name":[],
     "Odigrane":[],
     "Pobeda":[],
     "Poraz":[],
@@ -64,7 +44,7 @@ for sheet_name, df in dfs.items():
     
         # Calculate the correct values
         for j in range(1,len(df.iloc[0])):
-            if df.iloc[i,j] != '':
+            if df.iloc[i,j]:
                 played+=1
                 victory+=int(df.iloc[i+1,j]>df.iloc[i,j])
                 defeat+=int(df.iloc[i+1,j]<df.iloc[i,j])
@@ -72,13 +52,13 @@ for sheet_name, df in dfs.items():
                 losgames+=int(df.iloc[i,j])
         diff = wingames-losgames
 
-        # Special Case
-        #if played == 0:
-            #i+=2
-            #continue
+        # Special Case Uncomment the following code when you don't want teams that never played.
+        # if played == 0:
+        #     i+=2
+        #     continue
 
         # Assign the values to the dict
-        newdf["Igrac"].append(df.iloc[i,0])
+        newdf["Name"].append(df.iloc[i,0])  
         newdf["Odigrane"].append(played)
         newdf["Pobeda"].append(victory)
         newdf["Poraz"].append(defeat)
@@ -92,6 +72,7 @@ for sheet_name, df in dfs.items():
     # Create a DataFrame from the dict
     result = pd.DataFrame(newdf).sort_values(by=["Pobeda","Razlika gemova"],ascending=[False, False]).reset_index(drop=True)
     result.index += 1
-    display(result)  # if you use jupyter run display(result)
+    print(result)
         
     print("\n\n")
+    
