dd62d514a4f32f9a60601f9e69728b13aedf3081
milanpetrovic87 <50910654+milanpetrovic87@users.noreply.github.com>
2023-09-26 10:15:40 +0200

    Update parser_for_google_sheets.py

--- parser_for_google_sheets.py
+++ parser_for_google_sheets.py
@@ -44,7 +44,7 @@ for sheet_name, df in dfs.items():
     
         # Calculate the correct values
         for j in range(1,len(df.iloc[0])):
-            if df.iloc[i,j]:
+            if df.iloc[i,j] != '':
                 played+=1
                 victory+=int(df.iloc[i+1,j]>df.iloc[i,j])
                 defeat+=int(df.iloc[i+1,j]<df.iloc[i,j])
